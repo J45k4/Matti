396ce9105c4d26e8fee8de2d3f9a072b6919b711
Jaska <aa>
2016-01-27 16:05:49 +0200

    Main created

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "main.h"
+
+using namespace std;
+using namespace Wt;
+
+
+int main(int argc, char **argv)
+{
+    Wt::WServer server(argv[0]);
+    server.setServerConfiguration(argc, argv, WTHTTP_CONFIGURATION);
+    //server.addEntryPoint(Wt::Application, createRootApplication, "/");
+    server.addResource(new API(), "/");
+    if (server.start()) {
+      int sig = Wt::WServer::waitForShutdown(argv[0]);
+      std::cerr << "Shutdown (signal = " << sig << ")" << std::endl;
+      server.stop();
+    }
+    return 0;  
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <Wt/WApplication>
+#include <Wt/WServer>
+#include "api.h"
+
+#endif // MAIN_H
