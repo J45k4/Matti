f2b2c0c8cecba47c5b38da7aed3092dfa6bf2478
Jaska <ei.kuulu@sulle.t>
2016-12-15 23:16:28 +0200

    Lisätty MatrixConnectioniin tarvittavia metodien prototyyppejä.

--- Matti/IMatrixConnection.cpp
+++ Matti/IMatrixConnection.cpp
@@ -7,6 +7,58 @@ IMatrixConnection::IMatrixConnection()
 }
 
 
+IMatrixConnection::IMatrixConnection(string ip, int port, int numOfCons, int numOfCpu)
+{
+}
+
 IMatrixConnection::~IMatrixConnection()
+{
+}
+
+void IMatrixConnection::destroy()
+{
+}
+
+void IMatrixConnection::setVideo(int con, int cpu)
+{
+}
+
+void IMatrixConnection::setKwm(int con, int cpu)
+{
+}
+
+void IMatrixConnection::turnConPortOff(int con)
+{
+}
+
+void IMatrixConnection::turnCpuPortOff(int cpu)
+{
+}
+
+void IMatrixConnection::turnOffAllConPorts()
+{
+}
+
+void IMatrixConnection::turnOffAllCpuPorts()
+{
+}
+
+void IMatrixConnection::setAllVideo(int cons[], int cpus[])
+{
+}
+
+void IMatrixConnection::disconnectCon(int con)
+{
+}
+
+void IMatrixConnection::setMatrixCompleteSetup(int values)
+{
+}
+
+void IMatrixConnection::reset()
+{
+}
+
+void IMatrixConnection::requestAllStates(function<void()> callback)
 {
 }
--- Matti/IMatrixConnection.h
+++ Matti/IMatrixConnection.h
@@ -3,15 +3,36 @@
 #define IMATRIXCONNECTION_H
 
 #include <vector>
+#include <cstdint>
+#include <functional>
+#include <string>
 
 using namespace std;
 
 class IMatrixConnection
 {
-	
+	virtual int readNonBlock() = 0;
+	virtual void disconnect() = 0;
+	virtual void sendBuffer(unsigned char *buffer) = 0;
 public:
-	IMatrixConnection();
+	IMatrixConnection(string ip, int port, int numOfCons, int numOfCpu);
 	~IMatrixConnection();
+
+	void destroy();
+	void setVideo(int con, int cpu);
+	void setKwm(int con, int cpu);
+	void turnConPortOff(int con);
+	void turnCpuPortOff(int cpu);
+	void turnOffAllConPorts();
+	void turnOffAllCpuPorts();
+	void setAllVideo(int cons[], int cpus[]);
+	void disconnectCon(int con);
+	void setMatrixCompleteSetup(int values);
+	void reset();
+	void requestAllStates(function<void()> callback);
+
+
+
 };
 
 
